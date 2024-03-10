73acfa5c3691455e9c474b93a50c20cc6a1abc07
shannonjohn <shanjohncantz@gmail.com>
2024-03-11 01:53:36 +0800

    implemented integration testing for author

--- src/test/java/com/example/database/dao/AuthorDaoImplIntegrationTests.java
+++ src/test/java/com/example/database/dao/AuthorDaoImplIntegrationTests.java
@@ -5,8 +5,11 @@ import com.example.database.dao.impl.AuthorDaoImpl;
 import com.example.database.domain.Author;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static  org.assertj.core.api.Assertions.assertThat;
+import java.util.Optional;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
@@ -14,12 +17,19 @@ public class AuthorDaoImplIntegrationTests {
 
     private AuthorDaoImpl underTest;
 
+    @Autowired
     public AuthorDaoImplIntegrationTests(AuthorDaoImpl underTest){
         this.underTest = underTest;
     }
     @Test
     public void TestThatAuthorIsCreated(){
+
         Author author = TestDataUtil.createTestAuthor();
+        underTest.create(author);
+        Optional<Author> result = underTest.findOne(author.getId());
+        assertThat(result).isPresent();
+        assertThat(result.get()).isEqualTo(author);
+
     }
 
 
