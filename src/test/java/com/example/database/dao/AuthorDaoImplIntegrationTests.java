a2a46b42f0692f3f3506604d66a2cbe2fd57dbb8
shannonjohn <shanjohncantz@gmail.com>
2024-03-12 01:33:50 +0800

    added a "DirtiesContext" annotation on Author integration testing

--- src/test/java/com/example/database/dao/AuthorDaoImplIntegrationTests.java
+++ src/test/java/com/example/database/dao/AuthorDaoImplIntegrationTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import static  org.assertj.core.api.Assertions.assertThat;
 
@@ -15,6 +16,7 @@ import java.util.Optional;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class AuthorDaoImplIntegrationTests {
 
     private AuthorDaoImpl underTest;
