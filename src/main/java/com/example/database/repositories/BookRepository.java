8ec3923c222c469f4ac07aed4f405b8e61a203ac
shannonjohn <shanjohncantz@gmail.com>
2024-03-28 22:27:14 +0800

    implemented crud repo in book repo. refactored book repo test

--- src/main/java/com/example/database/repositories/BookRepository.java
+++ src/main/java/com/example/database/repositories/BookRepository.java
@@ -1,4 +1,10 @@
 package com.example.database.repositories;
 
-public interface BookRepository {
+
+import com.example.database.domain.Book;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends CrudRepository<Book, String> {
 }
--- src/test/java/com/example/database/repositories/BookRepositoryTests.java
+++ src/test/java/com/example/database/repositories/BookRepositoryTests.java
@@ -1,45 +1,45 @@
-//package com.example.database.repositories;
-//
-//import com.example.database.TestDataUtil;
-//import com.example.database.dao.impl.BookDaoImpl;
-//import com.example.database.domain.Author;
-//import com.example.database.domain.Book;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.annotation.DirtiesContext;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//import static  org.assertj.core.api.Assertions.assertThat;
-//@SpringBootTest
-//@ExtendWith(SpringExtension.class)
-//@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-//public class BookDaoImplIntegrationTests {
-//    private final BookDaoImpl underTest;
-//    private final AuthorDao authorDao;
-//
-//    @Autowired
-//    public BookDaoImplIntegrationTests(BookDaoImpl underTest, AuthorDao authorDao){
-//        this.underTest = underTest;
-//        this.authorDao = authorDao;
-//    }
-//
-//    @Test
-//    public void TestThatBookIsCreated(){
-//
-//        Author author = TestDataUtil.createTestAuthor1();
-//        authorDao.create(author);
-//        Book book = TestDataUtil.createBookA();
-//        book.setAuthorId(author.getId());
-//        underTest.create(book);
-//        Optional<Book> result = underTest.findOne(book.getIsbn());
-//        assertThat(result).isPresent();
-//        assertThat(result.get()).isEqualTo(book);
-//    }
+package com.example.database.repositories;
+
+import com.example.database.TestDataUtil;
+import com.example.database.repositories.AuthorRepositoryTests;
+import com.example.database.domain.Author;
+import com.example.database.domain.Book;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static  org.assertj.core.api.Assertions.assertThat;
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class BookRepositoryTests {
+    private final BookRepository underTest;
+    private final AuthorRepository authorDao;
+
+    @Autowired
+    public BookRepositoryTests(BookRepository underTest, AuthorRepository authorDao){
+        this.underTest = underTest;
+        this.authorDao = authorDao;
+    }
+
+    @Test
+    public void TestThatBookIsCreated(){
+
+        Author author = TestDataUtil.createTestAuthor1();
+        authorDao.save(author);
+        Book book = TestDataUtil.createBookA(author);
+        book.setAuthor(author);
+        underTest.save(book);
+        Optional<Book> result = underTest.findById(book.getIsbn());
+        assertThat(result).isPresent();
+        assertThat(result.get()).isEqualTo(book);
+    }
 //
 //    @Test
 //    public void TestThatBooksAreFetchedAfterCreation(){
@@ -95,5 +95,5 @@
 //        assertThat(result).isEmpty();
 //
 //    }
-//
-//}
+
+}
