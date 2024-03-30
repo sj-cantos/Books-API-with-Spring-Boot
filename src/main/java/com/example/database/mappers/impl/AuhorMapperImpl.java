0cf1e2e3eef369172097397de7bc1c839a3d8092
shannonjohn <shanjohncantz@gmail.com>
2024-03-31 01:58:43 +0800

    made actual implementation of AuthorMapper

--- src/main/java/com/example/database/mappers/impl/AuhorMapperImpl.java
+++ src/main/java/com/example/database/mappers/impl/AuhorMapperImpl.java
@@ -3,15 +3,18 @@ package com.example.database.mappers.impl;
 import com.example.database.domain.dto.AuthorDto;
 import com.example.database.domain.entities.AuthorEntity;
 import com.example.database.mappers.AuthorMapper;
+import org.modelmapper.ModelMapper;
 
 public class AuhorMapperImpl implements AuthorMapper<AuthorEntity, AuthorDto> {
+
+    private ModelMapper modelMapper;
     @Override
     public AuthorDto mapTo(AuthorEntity authorEntity) {
-        return null;
+        return modelMapper.map(authorEntity,AuthorDto.class);
     }
 
     @Override
     public AuthorEntity mapFrom(AuthorDto authorDto) {
-        return null;
+        return modelMapper.map(authorDto, AuthorEntity.class);
     }
 }
