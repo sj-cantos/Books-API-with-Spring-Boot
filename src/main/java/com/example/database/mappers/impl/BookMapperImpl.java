4bc7d90dcec26b956c2c42b3d2c0f93a8ec927c8
shannonjohn <shanjohncantz@gmail.com>
2024-04-01 03:01:31 +0800

    configured book mapper implementation

---
+++ src/main/java/com/example/database/mappers/impl/BookMapperImpl.java
@@ -0,0 +1,27 @@
+package com.example.database.mappers.impl;
+
+import com.example.database.domain.dto.BookDto;
+import com.example.database.domain.entities.BookEntity;
+import com.example.database.mappers.Mapper;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BookMapperImpl implements Mapper<BookEntity, BookDto> {
+
+    private ModelMapper modelMapper;
+
+    public BookMapperImpl(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public BookDto mapTo(BookEntity bookEntity) {
+        return modelMapper.map(bookEntity, BookDto.class);
+    }
+
+    @Override
+    public BookEntity mapFrom(BookDto bookDto) {
+        return modelMapper.map(bookDto, BookEntity.class);
+    }
+}
