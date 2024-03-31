88084a19b1bd486186fc6873111a2069c8d35ae6
shannonjohn <shanjohncantz@gmail.com>
2024-04-01 03:01:01 +0800

    created book creation endpoint

--- src/main/java/com/example/database/controllers/BookController.java
+++ src/main/java/com/example/database/controllers/BookController.java
@@ -1,4 +1,30 @@
 package com.example.database.controllers;
 
+import com.example.database.domain.dto.AuthorDto;
+import com.example.database.domain.dto.BookDto;
+import com.example.database.domain.entities.BookEntity;
+import com.example.database.mappers.Mapper;
+import com.example.database.mappers.impl.BookMapperImpl;
+import com.example.database.services.BookService;
+import com.example.database.services.impl.BookServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class BookController {
+    private BookService bookService;
+    private Mapper<BookEntity, BookDto> bookMapper;
+
+    public BookController(BookService bookService,Mapper<BookEntity, BookDto> bookMapper) {
+        this.bookService = bookService;
+        this.bookMapper = bookMapper;
+    }
+
+    @PutMapping(path = "/books/{isbn}")
+    public ResponseEntity<BookDto> createBook(@PathVariable("isbn") String isbn, @RequestBody BookDto bookDto){
+        BookEntity bookEntity = bookMapper.mapFrom(bookDto);
+        BookEntity savedBookEntity = bookService.save(bookEntity,isbn);
+        return new ResponseEntity<>(bookMapper.mapTo(savedBookEntity),HttpStatus.CREATED);
+    }
 }
