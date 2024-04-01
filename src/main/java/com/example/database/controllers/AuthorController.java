41c015497b8ed4c7d779bcc0b8189901627503a7
shannonjohn <shanjohncantz@gmail.com>
2024-04-02 02:23:29 +0800

    created find all authors endpoint

--- src/main/java/com/example/database/controllers/AuthorController.java
+++ src/main/java/com/example/database/controllers/AuthorController.java
@@ -6,9 +6,13 @@ import com.example.database.mappers.Mapper;
 import com.example.database.services.AuthorService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class AuthorController {
@@ -26,5 +30,13 @@ public class AuthorController {
         AuthorEntity authorEntity = authorMapper.mapFrom(author);
         AuthorEntity savedAuthorEntity = authorService.save(authorEntity);
         return new ResponseEntity<>(authorMapper.mapTo(savedAuthorEntity),HttpStatus.CREATED);
+    }
+    @GetMapping(path = "/authors")
+    public List<AuthorDto> findAllAuthors(){
+        List<AuthorEntity> authorEntityList = authorService.findAll();
+        return authorEntityList.stream()
+                .map(authorMapper::mapTo)
+                .collect(Collectors.toList());
+
     }
 }
