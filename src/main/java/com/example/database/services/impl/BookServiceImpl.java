2625fccaf12089e26a06c2b83b831974ca189d58
shannonjohn <shanjohncantz@gmail.com>
2024-04-02 02:30:33 +0800

    implemented find all books method

--- src/main/java/com/example/database/services/impl/BookServiceImpl.java
+++ src/main/java/com/example/database/services/impl/BookServiceImpl.java
@@ -5,6 +5,10 @@ import com.example.database.repositories.BookRepository;
 import com.example.database.services.BookService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 @Service
 public class BookServiceImpl implements BookService {
     private BookRepository bookRepository;
@@ -17,5 +21,10 @@ public class BookServiceImpl implements BookService {
     public BookEntity save(BookEntity bookEntity, String isbn) {
         bookEntity.setIsbn(isbn);
         return bookRepository.save(bookEntity);
+    }
+
+    @Override
+    public List<BookEntity> findAll() {
+        return StreamSupport.stream(bookRepository.findAll().spliterator(),false).collect(Collectors.toList());
     }
 }
