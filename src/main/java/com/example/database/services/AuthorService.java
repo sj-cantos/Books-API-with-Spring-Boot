62d1069c339677bd3e0d84f47031be8d805f233e
shannonjohn <shanjohncantz@gmail.com>
2024-04-02 02:22:53 +0800

    initialised find all method

--- src/main/java/com/example/database/services/AuthorService.java
+++ src/main/java/com/example/database/services/AuthorService.java
@@ -2,7 +2,10 @@ package com.example.database.services;
 
 import com.example.database.domain.entities.AuthorEntity;
 
+import java.util.List;
+
 
 public interface AuthorService {
     AuthorEntity save(AuthorEntity authorEntity);
+    List<AuthorEntity> findAll();
 }
