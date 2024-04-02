84a2f0c8fe3f17f8501c42bcee59dfe99744a9ea
shannonjohn <shanjohncantz@gmail.com>
2024-04-03 00:56:44 +0800

    initialised find all method

--- src/main/java/com/example/database/services/BookService.java
+++ src/main/java/com/example/database/services/BookService.java
@@ -3,10 +3,13 @@ package com.example.database.services;
 import com.example.database.domain.dto.BookDto;
 import com.example.database.domain.entities.BookEntity;
 
+import java.awt.print.Book;
 import java.util.List;
 
 public interface BookService
 {
     BookEntity save(BookEntity bookEntity, String isbn);
     List<BookEntity> findAll();
+
+    BookEntity findOne(String isbn);
 }
