ed0c0a7b20f109c139c63f83f248f7437e0a096a
shannonjohn <shanjohncantz@gmail.com>
2024-04-05 00:10:12 +0800

    changed return type to optional

--- src/main/java/com/example/database/services/AuthorService.java
+++ src/main/java/com/example/database/services/AuthorService.java
@@ -3,10 +3,11 @@ package com.example.database.services;
 import com.example.database.domain.entities.AuthorEntity;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface AuthorService {
     AuthorEntity save(AuthorEntity authorEntity);
     List<AuthorEntity> findAll();
-    AuthorEntity findOne(long id);
+    Optional<AuthorEntity> findOne(long id);
 }
