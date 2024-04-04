51717f54043cc73db4ce870fc77ee936643a9912
shannonjohn <shanjohncantz@gmail.com>
2024-04-04 23:37:12 +0800

    implemented find one author

--- src/main/java/com/example/database/services/impl/AuthorServiceImpl.java
+++ src/main/java/com/example/database/services/impl/AuthorServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -27,5 +28,14 @@ public class AuthorServiceImpl implements AuthorService {
     public List<AuthorEntity> findAll() {
         Iterable<AuthorEntity> authors = authorRepository.findAll();
         return StreamSupport.stream(authors.spliterator(),false).collect(Collectors.toList());
+    }
+
+    @Override
+    public AuthorEntity findOne(long id) {
+        Optional<AuthorEntity> author = authorRepository.findById(id);
+        if(author.isPresent()){
+            return author.get();
+        }
+        return null;
     }
 }
