d132a9a23b7386a0cc18f53894df90d7c4e20899
shannonjohn <shanjohncantz@gmail.com>
2024-04-07 17:27:55 +0800

    declared find by authors id function

--- src/main/java/com/example/database/repositories/BookRepository.java
+++ src/main/java/com/example/database/repositories/BookRepository.java
@@ -11,5 +11,5 @@ import java.util.Optional;
 
 @Repository
 public interface BookRepository extends CrudRepository<BookEntity, String> {
-
+    List<BookEntity> findByAuthorEntityId(Long authorId);
 }
